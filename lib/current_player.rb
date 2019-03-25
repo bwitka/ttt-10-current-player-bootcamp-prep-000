3c4d53859999180aed94ccc1e1c7d663513d5709
BEAU WITKA <bwitka@gmail.com>
2019-03-25 17:16:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,18 +9,12 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
+
+  # if turn_count(board) % 2 == 0 
+  #   return "X"
+  # else
+  #   return "O"
+  # end
 end
 
-input_type = turn_count(board) % 2 == 0 ? "X" : "O"
-
-input_type =
-  if gets =~ /hello/i
-    "greeting"
-  else
-    "other"
-  end
